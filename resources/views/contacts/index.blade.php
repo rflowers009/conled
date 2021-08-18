b2d47717a0d7ff2fed5c03587bce2c213219307c
Russell Flowers <rflowers@gmail.com>
2021-08-18 10:58:38 -0500

    Just a title update

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -7,7 +7,7 @@
     <div class="row">   
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Laravel 8 CRUD Example </h2>
+                <h2>Contacts</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-success" href="/contacts/create" title="Create a contact"> <i class="fas fa-plus-circle"></i>
